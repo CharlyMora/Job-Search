803e68540b35bd92d2e22eb1ae3d6350de2a15ba
CharlyMora <morandres.carlos@gmail.com>
2021-03-12 21:52:17 -0500

    jcommander ready

--- src/main/java/com/andres/jobsearch/JobSearch.java
+++ src/main/java/com/andres/jobsearch/JobSearch.java
@@ -1,7 +1,13 @@
 package com.andres.jobsearch;
 
+import com.andres.jobsearch.cli.CLIArguments;
+import com.beust.jcommander.JCommander;
+
+import static com.andres.jobsearch.CommanderFunctions.buildCommanderWithName;
+
 public class JobSearch {
     public static void main(String[] args) {
         System.out.println("Hello job search");
+        JCommander jCommander= buildCommanderWithName("job-search", CLIArguments::newInstance);
     }
 }
